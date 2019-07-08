af1f1c883080ce97d4f0228ed69fceabf34a6e8f
Akshay Kumar S <akshay@mitsogo.com>
2019-07-08 23:12:26 +0530

    root status image changed

--- app/src/main/java/com/twoxmars/rootchecker/adapter/MainAdapter.kt
+++ app/src/main/java/com/twoxmars/rootchecker/adapter/MainAdapter.kt
@@ -4,9 +4,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.nightonke.jellytogglebutton.JellyToggleButton
 import com.twoxmars.rootchecker.R
 import com.twoxmars.rootchecker.model.RootCheckInfo
 
@@ -37,11 +37,13 @@ class MainAdapter(private val rootCheckInfo: ArrayList<RootCheckInfo>) : Recycle
         fun bindItems(info: RootCheckInfo) {
             val title = itemView.findViewById(R.id.tv_method_title) as TextView
             val content = itemView.findViewById(R.id.tv_method_content) as TextView
-            val result = itemView.findViewById(R.id.checkbox) as JellyToggleButton
+            val result = itemView.findViewById(R.id.success_failed) as ImageView
             title.text = info.title
             content.text = info.content
-            result.isChecked = info.result
-            result.isClickable = false
+            if (info.result)
+                result.setImageResource(R.drawable.ic_check_24dp)
+            else
+                result.setImageResource(R.drawable.ic_close_24dp)
         }
     }
 }
